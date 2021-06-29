0d12e9c410027e4ba898e55e724b390b715b2409
Vineet <2018uec1733@mnit.ac.in>
2021-06-29 13:24:58 +0530

    final build files
    up and running on netlify

---
+++ client/build/precache-manifest.fc29dff98c98b9d82d2bd4726e6c18af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54afea24f19fe53bb4deca682e5e955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7471441d079476c13725",
+    "url": "/static/css/main.be34dabf.chunk.css"
+  },
+  {
+    "revision": "6bfdc4776029d4435080",
+    "url": "/static/js/2.890f412a.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.890f412a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7471441d079476c13725",
+    "url": "/static/js/main.d87996cc.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "9007e0d871bdd31e50d4dd5ce3bd72aa",
+    "url": "/static/media/bistrologo.9007e0d8.png"
+  },
+  {
+    "revision": "3e5bc30aafb1947d35b20fc65b3fed56",
+    "url": "/static/media/bistrotext.3e5bc30a.png"
+  }
+]);
